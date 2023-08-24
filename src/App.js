5bb0fc69c1282d54f2f5174d7ebc2abe88aaa70e
agmatyagi1717 <agamtyagi1717@gmail.com>
2023-08-25 00:05:52 +0530

    favicon

--- src/App.js
+++ src/App.js
@@ -22,9 +22,15 @@ function App() {
           <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
             Speech to Text Convertor
           </h1>
-          <p className="leading-relaxed mb-8">
+          <p className="leading-relaxed mb-3">
             Made using ReactJS and Tailwind CSS
           </p>
+          
+          <label class="relative inline-flex items-center cursor-pointer mb-5">
+            <input type="checkbox" value="" class="sr-only peer" />
+            <div class="w-11 h-6 bg-gray-200 rounded-full peer peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-indigo-500"></div>
+            <span class="ml-3 text-sm font-medium text-gray-900 dark:text-gray-300">Lightmode</span>
+          </label>
 
           <div className="flex justify-center">
             <button onClick={startListening} className="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
@@ -38,7 +44,7 @@ function App() {
           </div>
           <div className=" container bg-gray-800 border border-indigo-500 mx-auto flex items-center justify-center flex-col my-8">
             {transcript}
-          </div>
+          </div>          
 
           <p className="leading-relaxed mb-8">
            © Agam Tyagi
