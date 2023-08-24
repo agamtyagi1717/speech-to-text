ab0626878b936281827e9e5e0938923fee471803
agmatyagi1717 <agamtyagi1717@gmail.com>
2023-08-24 23:16:31 +0530

    logo

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
+import logo from './assets/logo.png'
 
 function App() { 
   const { transcript, browserSupportsSpeechRecognition } = useSpeechRecognition();
@@ -15,7 +16,7 @@ function App() {
     <section className="text-gray-400 bg-gray-900 body-font">
       <div className="container mx-auto flex px-5 py-24 items-center justify-center flex-col">
 
-        {/* <img className="lg:w-2/6 md:w-3/6 w-5/6 mb-10 object-cover object-center rounded" alt="hero" src="https://dummyimage.com/720x600" /> */}
+        <img className="lg:w-1/12 md:w-1/6 w-2/6 mb-10 object-cover object-center rounded" alt="hero" src={logo} />
 
         <div className="text-center lg:w-2/3 w-full">
           <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
